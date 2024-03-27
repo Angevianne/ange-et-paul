2605e0c9de83b5260bfeffb90090b2952fe6293f
Paul EHLINGER <paul.ehlinger@cdfnancy.fr>
2024-03-27 14:04:02 +0100

    code finit

--- codeYoupi.cpp
+++ codeYoupi.cpp
@@ -1,29 +1,45 @@
-const int step_delay = 100; // Définition de la constante pour le délai entre chaque pas
-const int base_pins[8] = {2, 3, 4, 5, 6, 7, 8, 9}; // Broches utilisées pour contrôler le moteur
+//*************************BONJOUR MONSIEUR ROBERT, VOICI LE MAGNIFIQUE CODE POUR FAIRE TOURNER LA BASE.************************************************************************
+//*************************LE PROBLEME N'ETAIT PAS DANS LES PINS, MAIS DANS LE DEUXIEME "STEP_SEQUENCE"*************************************************************************
+const int step_delay = 10; 
+const int base_pins[8] = {2, 3, 4, 5, 6, 7, 8, 9}; 
+
+const int D6=8;
 
 void setup() {
+  int sens_step_sequence[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+  int step_sequence_1[8] = {0, 1, 0, 0, 0, 0, 0, 0};
+  int step_sequence_2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
   for (int i = 0; i < 8; i++) {
-    pinMode(base_pins[i], OUTPUT); // Définition des broches de sortie
+    pinMode(base_pins[i], OUTPUT);
+  }
+
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(base_pins[i],sens_step_sequence[i]);
+  }
+  delay(50);
+  // pour définir le numéro du moteur
+  for (int j = 0; j < 8; j++) 
+  {
+    digitalWrite(base_pins[j], step_sequence_1[j]);
+  }
+  for (int j = 0; j < 8; j++) 
+  {
+    digitalWrite(base_pins[j], step_sequence_2[j]);
   }
 }
 
 void loop() {
-  moveOneStep(); // Appel de la fonction pour effectuer un pas
-  delay(step_delay); // Attente avant le prochain pas
+  moveOneStep(); 
+  delay(step_delay); 
 }
 
 void moveOneStep() {
-  // Définition des séquences de pas
-  int step_sequence_1[8] = {0, 1, 0, 0, 0, 0, 0, 0};
-  int step_sequence_2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-  // Boucle pour effectuer un pas
-  for (int i = 0; i < 100; i++) { // Répéter le mouvement 100 fois
-    for (int j = 0; j < 8; j++) {
-      // Envoyer les signaux de la séquence de pas aux broches correspondantes
-      digitalWrite(base_pins[j], step_sequence_1[j]);
-      delay(10); // Délai entre chaque étape du pas
-    }
-    delay(step_delay); // Délai entre chaque répétition de pas
+    digitalWrite(D6,LOW);
+    delay(10);
+    digitalWrite(D6,HIGH);
+    delay(10);
+      
   }
-}
+
