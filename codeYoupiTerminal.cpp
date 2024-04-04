4f22bcf29c500dc2340333412c2ccf4c16b75130
Paul EHLINGER <paul.ehlinger@cdfnancy.fr>
2024-04-04 09:19:28 +0200

    caca

--- codeYoupiTerminal.cpp
+++ codeYoupiTerminal.cpp
@@ -1,43 +1,44 @@
-const int step_delay = 10; 
-const int base_pins[8] = {2, 3, 4, 5, 6, 7, 8, 9}; 
+const int step_delay = 10;
+const int base_pins[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-const int D6=8;
+const int control_pin = 10;
 
 void setup() {
-  int sens_step_sequence[8] = {1, 0, 0, 0, 0, 0, 0, 0};
-  int step_sequence_1[8] = {0, 1, 0, 0, 0, 0, 0, 0};
-  int step_sequence_2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
   for (int i = 0; i < 8; i++) {
     pinMode(base_pins[i], OUTPUT);
   }
 
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(base_pins[i],sens_step_sequence[i]);
-  }
-  delay(50);
-  for (int j = 0; j < 8; j++) 
-  {
-    digitalWrite(base_pins[j], step_sequence_1[j]);
-  }
-  for (int j = 0; j < 8; j++) 
-  {
-    digitalWrite(base_pins[j], step_sequence_2[j]);
-  }
+  pinMode(control_pin, INPUT);
+
+  moveOneStep();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    int bits = Serial.parseInt();
-    if (bits >= 0 && bits < 256) {
-      setOutputBits(bits);
-    }
+  if (digitalRead(control_pin) == HIGH) {
+    moveOneStep();
+    delay(step_delay);
   }
 }
 
-void setOutputBits(int bits) {
+void moveOneStep() {
+  static int step_sequence_index = 0;
+  int step_sequence[][8] = {
+    {1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1}
+  };
+
   for (int i = 0; i < 8; i++) {
-    digitalWrite(base_pins[i], bitRead(bits, i));
+    digitalWrite(base_pins[i], step_sequence[step_sequence_index][i]);
   }
-  delay(step_delay);
+
+  step_sequence_index++;
+  if (step_sequence_index >= 8) {
+    step_sequence_index = 0;
+  }
 }
