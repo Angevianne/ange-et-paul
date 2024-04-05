84b33426545dc53c757e39570dfc8e65517889b0
Paul EHLINGER <paul.ehlinger@cdfnancy.fr>
2024-04-05 11:05:47 +0200

    commit

--- codeYoupiTerminal.cpp
+++ codeYoupiTerminal.cpp
@@ -1,44 +1,54 @@
-const int step_delay = 10;
-const int base_pins[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+//*************************BONJOUR MONSIEUR ROBERT, VOICI LE MAGNIFIQUE CODE POUR FAIRE TOURNER LA BASE.************************************************************************
+//*************************LE PROBLEME N'ETAIT PAS DANS LES PINS, MAIS DANS LE DEUXIEME "STEP_SEQUENCE"*************************************************************************
+const int step_delay = 10; 
+const int base_pins[8] = {2, 3, 4, 5, 6, 7, 8, 9}; 
 
-const int control_pin = 10;
+const int D6=8;
 
 void setup() {
+  int sens_step_sequence[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+  int step_sequence_1[8] = {0, 1, 0, 0, 0, 0, 0, 0};
+  int step_sequence_2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
   for (int i = 0; i < 8; i++) {
     pinMode(base_pins[i], OUTPUT);
   }
 
-  pinMode(control_pin, INPUT);
-
-  moveOneStep();
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(base_pins[i],sens_step_sequence[i]);
+  }
+  delay(50);
+  // pour définir le numéro du moteur
+  for (int j = 0; j < 8; j++) 
+  {
+    digitalWrite(base_pins[j], step_sequence_1[j]);
+  }
+  for (int j = 0; j < 8; j++) 
+  {
+    digitalWrite(base_pins[j], step_sequence_2[j]);
+  }
 }
 
 void loop() {
-  if (digitalRead(control_pin) == HIGH) {
-    moveOneStep();
-    delay(step_delay);
+  int nbPas;
+  Serial.begin(9600);
+  Serial.println("Entrez le nombre de pas à effectuer :");
+  while(!Serial.available());
+  nbPas = Serial.parseInt();
+  Serial.print("Nombre de pas à effectuer : ");
+  Serial.println(nbPas);
+  
+  for(int i=0; i<nbPas; i++) {
+    moveOneStep(); 
+    delay(step_delay); 
   }
 }
 
 void moveOneStep() {
-  static int step_sequence_index = 0;
-  int step_sequence[][8] = {
-    {1, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 1}
-  };
 
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(base_pins[i], step_sequence[step_sequence_index][i]);
+    digitalWrite(D6,LOW);
+    delay(10);
+    digitalWrite(D6,HIGH);
+    delay(10);
+      
   }
-
-  step_sequence_index++;
-  if (step_sequence_index >= 8) {
-    step_sequence_index = 0;
-  }
-}
